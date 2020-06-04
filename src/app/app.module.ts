32756882b34512b962bc258f3aab4374e7e1e0b3
chiarimassetti <chiarimassetti@gmail.com>
2020-06-04 12:44:57 -0300

    4 files modified, 11 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MenuComponent } from './menu/menu.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { ArticulosListaComponent } from './articulos-lista/articulos-lista.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, MenuComponent, PrincipalComponent, ArticulosListaComponent ],
+  bootstrap:    [ PrincipalComponent ]
 })
 export class AppModule { }
